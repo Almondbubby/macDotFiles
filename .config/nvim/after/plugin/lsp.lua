e82a9504a744be325a358e23e4d20e4dc556db8a
Raymond Fu <almondbubby@Raymonds-MacBook-Air.local>
2023-05-14 12:37:11 -0400

    initial

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,21 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+lsp.ensure_installed({
+	'pyright',
+	'clangd',
+	'jdtls',
+
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<Tab>'] = cmp.mapping.confirm({select = true}),
+})
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
+
+lsp.setup()
